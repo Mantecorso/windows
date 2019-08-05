180f33e1f4617781181e0c734d1ac7d5560e81b3
Mantecorso <luisjuradoquesada@gmail.com>
2019-08-05 16:36:35 +0200

    css and js ready

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
-alert('Mensaje de prueba');
+
+function poner1() {
+
+    document.getElementById('dos').style.backgroundColor = "#1e1e1e";
+    document.getElementById('nume1').style.backgroundColor = "rgb(180, 60, 60)";
+    document.getElementById('nume2').style.backgroundColor = "#4680a1";
+    document.getElementById('nume3').style.backgroundColor = "#2e7c1e";
+    document.getElementById('nume4').style.backgroundColor = "#6b8118";
+    document.getElementById('dos').style.zIndex = "10";
+    document.getElementById('nume1').style.zIndex = "6";
+    document.getElementById('nume2').style.zIndex = "5";
+    document.getElementById('nume3').style.zIndex = "4";
+    document.getElementById('nume4').style.zIndex = "3";
+}
+
+    function poner2() {
+        
+        document.getElementById('dos').style.backgroundColor = "#ad75d8";
+        document.getElementById('nume1').style.backgroundColor = "#1e1e1e";
+        document.getElementById('nume2').style.backgroundColor = "#4680a1";
+        document.getElementById('nume3').style.backgroundColor = "#2e7c1e";
+        document.getElementById('nume4').style.backgroundColor = "#6b8118";
+        document.getElementById('dos').style.zIndex = "2";
+        document.getElementById('nume1').style.zIndex = "10";
+        document.getElementById('nume2').style.zIndex = "5";
+        document.getElementById('nume3').style.zIndex = "4";
+        document.getElementById('nume4').style.zIndex = "3";
+    }
+
+    function poner3() {
+        
+        document.getElementById('dos').style.backgroundColor = "#ad75d8";
+        document.getElementById('nume1').style.backgroundColor = "rgb(180, 60, 60)";
+        document.getElementById('nume2').style.backgroundColor = "#1e1e1e";
+        document.getElementById('nume3').style.backgroundColor = "#2e7c1e";
+        document.getElementById('nume4').style.backgroundColor = "#6b8118";
+        document.getElementById('dos').style.zIndex = "2";
+        document.getElementById('nume1').style.zIndex = "3";
+        document.getElementById('nume2').style.zIndex = "6";
+        document.getElementById('nume3').style.zIndex = "5";
+        document.getElementById('nume4').style.zIndex = "4";
+    }
+
+    function poner4() {
+        
+        document.getElementById('dos').style.backgroundColor = "#ad75d8";
+        document.getElementById('nume1').style.backgroundColor = "rgb(180, 60, 60)";
+        document.getElementById('nume2').style.backgroundColor = "#4680a1";
+        document.getElementById('nume3').style.backgroundColor = "#1e1e1e";
+        document.getElementById('nume4').style.backgroundColor = "#6b8118";
+        document.getElementById('dos').style.zIndex = "2";
+        document.getElementById('nume1').style.zIndex = "3";
+        document.getElementById('nume2').style.zIndex = "4";
+        document.getElementById('nume3').style.zIndex = "6";
+        document.getElementById('nume4').style.zIndex = "5";
+    }
+
+    function poner5() {
+        
+        document.getElementById('dos').style.backgroundColor = "#ad75d8";
+        document.getElementById('nume1').style.backgroundColor = "rgb(180, 60, 60)";
+        document.getElementById('nume2').style.backgroundColor = "#4680a1";
+        document.getElementById('nume3').style.backgroundColor = "#2e7c1e";
+        document.getElementById('nume4').style.backgroundColor = "#1e1e1e";
+        document.getElementById('dos').style.zIndex = "2";
+        document.getElementById('nume1').style.zIndex = "3";
+        document.getElementById('nume2').style.zIndex = "4";
+        document.getElementById('nume3').style.zIndex = "5";
+        document.getElementById('nume4').style.zIndex = "6";
+    }
